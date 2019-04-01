9d3642e1b3b6f649ea01dd930406f15585b0f2fe
ouyounger <ouyounger@163.com>
2019-04-01 11:38:27 +0800

    set env in .py file

---
+++ python/hello_world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# this comment clear .py file can be executed by double click  
+# but should "chmod a+x filename.py" first 
+# comment begin with #
+# comments in ''' '''
+print("hello world")
