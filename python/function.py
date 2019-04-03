0b98f292af7ba06e7ba915fd04d910e90b47c8c9
ouyounger <ouyounger@163.com>
2019-04-03 19:36:24 +0800

    param in function, not easy to remember

--- python/function.py
+++ python/function.py
@@ -10,7 +10,8 @@ def nop():
     # pass means do nothing
     pass
 
-def add(i,j):
+# param j's default value is 1
+def add(i,j=1):
     if not isinstance(i,(int,float)):
         # throw exception
         raise TypeError("invalid param")
@@ -18,8 +19,8 @@ def add(i,j):
         raise TypeError("int needed")
     return i+j
 
-
-print(add(3,40))
+# use default param
+print(add(3))
 
 def MultiRtn():
     # return not 0,-1 exactly
@@ -29,4 +30,44 @@ rtn = MultiRtn()
 # function can return more than one result in a tuple, seems like more than one result
 print(rtn)
 
+# default params like a const pointer in c/c++
+# basic var is copy,while object is reference such as lst,dic,set etc.
+# the following fn() will append "12tall" to lst every called
+def fn(i=1,lst=[]):
+    i=i+1
+    lst.append("12tall")
+    print(i)
+    print(lst)
+    return
+fn()
+fn()
+
+# params[]
+# var parasm more like a tuple 
+# likes return multiresult
+def fn2(*p):
+    print(p)
+    return
+fn2(1,2,3)
+
+# keyword function
+def fn3(n,**kv):
+    print(n)
+    print(kv)
+    return
+# no same param name!!!
+fn3(1,m=2)
+
+# named keyword function
+# param after * is named
+# this type of function accept declared keyword param only
+def fn4(n,*,a):
+    print(n)
+    print(a)
+    return
+# must use a=*** when called
+fn4(1,a=2)
+
+# it's confusing
+# https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431752945034eb82ac80a3e64b9bb4929b16eeed1eb9000
 
