1b5cb8c6eb58844f3f5f4ae1712ebece6541f75f
ouyounger <ouyounger@163.com>
2019-04-11 18:47:16 +0800

    coroutine,abstract

---
+++ python/coroutine.py
@@ -0,0 +1,36 @@
+# https://blog.csdn.net/soonfly/article/details/78361819
+
+import asyncio,random
+@asyncio.coroutine
+def smart_fib(n):
+    index = 0
+    a = 0
+    b = 1
+    while index < n:
+        sleep_secs = random.uniform(0, 0.2)
+        yield from asyncio.sleep(sleep_secs) #通常yield from后都是接的耗时操作
+        print('Smart one think {} secs to get {}'.format(sleep_secs, b))
+        a, b = b, a + b
+        index += 1
+
+@asyncio.coroutine
+def stupid_fib(n):
+    index = 0
+    a = 0
+    b = 1
+    while index < n:
+        sleep_secs = random.uniform(0, 0.4)
+        yield from asyncio.sleep(sleep_secs) #通常yield from后都是接的耗时操作
+        print('Stupid one think {} secs to get {}'.format(sleep_secs, b))
+        a, b = b, a + b
+        index += 1
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    tasks = [
+        smart_fib(10),
+        stupid_fib(10),
+    ]
+    loop.run_until_complete(asyncio.wait(tasks))
+    print('All fib finished.')
+    loop.close()
