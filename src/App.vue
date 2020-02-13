c346423d267e9986ab672b7342f6e1e3d46f931c
12tall <fb.ouyang@outlook.com>
2020-02-13 16:04:26 +0100

    add a class like style

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,49 @@
   <div id="app">
     <el-container>
       <el-header height="30px"></el-header>
-      <el-aside width="50px"></el-aside>
+      <!-- css 设置不生效 style="padding:0" -->
+      <el-main>
+        <el-aside width="50px"></el-aside>
+      </el-main>
       <el-footer height="30px"></el-footer>
     </el-container>
   </div>
 </template>
 
 <style lang="stylus">
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0;
-  padding: 0;
 }
 
-.el-footer {
-  height: 100%;
-}
+.el-container {
+  .el-header {
+    background-color: blue;
+  }
 
-.header {
-  height: 30px;
-  display: flex;
-  margin: 0;
-  padding: 0;
+  .el-main {
+    padding: 0;
+
+    .el-aside {
+      background-color: green;
+    }
+  }
+
+  .el-footer {
+    background-color: red;
+  }
 }
+</style>
+<style>
+/* .el-main {
+  padding: 0;
+} */
 </style>
