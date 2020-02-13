03083924c233a138e3ce82807968730cd5607c5a
12tall <fb.ouyang@outlook.com>
2020-02-13 18:14:56 +0100

    modify button and use flex layout

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,23 @@
     <el-container>
       <el-header height="30px" style="-webkit-app-region:drag">
         <!-- no-drag 属性在css 设置里面不生效 -->
-        <span>toys</span>
-        <div style="-webkit-app-region: no-drag">
-          <v-icon @click="MenubarEnvent(2)" scale="1.5" name="window-minimize"></v-icon>
-          <v-icon @click="MenubarEnvent(1)" scale="1.5" name="window-maximize"></v-icon>
-          <v-icon @click="MenubarEnvent(0)" scale="1.5" name="window-close"></v-icon>
+        <div class="title">
+          <el-avatar shape="square" size="small" :src="logo"></el-avatar>
+          <span>toys</span>
+        </div>
+        <div class="button" style="-webkit-app-region: no-drag">
+          <i class="el-icon-arrow-down" @click="MenubarEnvent(2)"></i>
+          <i class="el-icon-arrow-up" @click="MenubarEnvent(1)"></i>
+          <i class="el-icon-close" @click="MenubarEnvent(0)"></i>
         </div>
       </el-header>
       <!-- css 设置不生效 style="padding:0" -->
       <el-main>
         <el-aside width="50px"></el-aside>
+        <el-container>
+          <el-aside width="150px"></el-aside>
+          <el-main></el-main>
+        </el-container>
       </el-main>
       <el-footer height="30px"></el-footer>
     </el-container>
@@ -23,7 +30,9 @@
 import { ipcRenderer } from 'electron';
 export default {
   data() {
-    return {};
+    return {
+      logo: require('@/assets/logo.png'),
+    };
   },
   methods: {
     MenubarEnvent(index) {
@@ -35,11 +44,6 @@ export default {
 
 
 <style lang="stylus">
-* {
-  margin: 0;
-  padding: 0;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -50,41 +54,67 @@ export default {
 
 // 推荐类风格样式
 .el-container {
+  height: 100%;
+
   .el-header {
-    background-color: gray;
+    color: #8f8f8f;
+    background-color: #333333;
+    padding: 0 10px 0 10px;
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
 
-    span {
+    .title {
       height: 20px;
-    }
+      display: flex;
 
-    div {
-      svg {
-        margin: 0 1px 0 1px;
-        padding: 0;
+      span {
+        padding: auto;
       }
 
-      svg:hover {
-        color: #000;
+      .el-avatar {
+        height: 20px;
+        width: 20px;
+        margin-right: 20px;
+        background-color: #333333;
+      }
+    }
+
+    .button {
+      i {
+        margin: 0 5px 0 1px;
+        font-weight: bolder;
+      }
+
+      i:hover {
+        color: #ffffff;
       }
     }
   }
 
   .el-main {
+    display: flex;
     padding: 0;
 
     .el-aside {
-      background-color: green;
+      background-color: #333333;
+    }
+
+    .el-container {
+      .el-aside {
+        background-color: #252525;
+      }
+
+      .el-main {
+        background-color: #1e1e1e;
+      }
     }
   }
 
   .el-footer {
-    background-color: red;
+    background-color: #333333;
   }
 }
 </style>
