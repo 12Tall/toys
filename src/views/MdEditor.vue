112c55441606f9f05fcb262c256cec9eb07a6c96
12tall <fb.ouyang@outlook.com>
2020-02-18 14:53:19 +0100

    add editor component and support drag to resize element, synchronize scroll

--- src/views/MdEditor.vue
+++ src/views/MdEditor.vue
@@ -2,8 +2,7 @@
   <el-container>
     <Index></Index>
     <el-main>
-      <textarea v-model="plain" v-on:scroll="EditorScroll"></textarea>
-      <div id="previewer" v-html="T"></div>
+      <Editor></Editor>
     </el-main>
   </el-container>
 </template>
@@ -11,6 +10,8 @@
 <script>
 import Vue from 'vue';
 import Index from '@/components/MdEditor/Index';
+import Editor from '@/components/MdEditor/Ediitor';
+
 import 'markdown-it-highlight/dist/index.css';
 
 import MarkdownIt from 'markdown-it';
@@ -18,15 +19,15 @@ import { default as ml } from 'markdown-it-highlight';
 import { default as mk } from 'markdown-it-latex';
 import 'markdown-it-latex/dist/index.css';
 const md = new MarkdownIt();
+md.enable(['link', 'image']);
 md.use(mk);
 md.use(ml);
 
+let editor, w;
 export default {
   name: 'Home',
   data() {
     return {
-      // md: new MarkdownIt(),
-      plain: '',
     };
   },
   computed: {
@@ -34,19 +35,9 @@ export default {
       return md.render(this.plain);
     },
   },
-  methods: {
-    EditorScroll(e) {
-      var editor = document.querySelector('textarea');
-      var previewer = document.querySelector('#previewer');
-      console.log(editor.scrollHeight / previewer.scrollHeight);
-      let scale =
-        (editor.scrollHeight - editor.clientHeight) /
-        (previewer.scrollHeight - previewer.clientHeight);
-      previewer.scrollTop = editor.scrollTop / scale;
-    },
-  },
   components: {
     Index,
+    Editor,
   },
 };
 </script>
@@ -62,23 +53,8 @@ export default {
   .el-container {
     .el-main {
       background-color: #1e1e1e;
-      display: flex;
       flex-direction: row;
       flex-wrap: nowrap;
-
-      textarea {
-        flex-grow: 1;
-        font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-      }
-
-      #previewer {
-        background-color: #ffffff;
-        padding-left: 20px;
-        margin: 0;
-        text-align: left;
-        flex-grow: 1;
-        overflow-y: scroll;
-      }
     }
   }
 }
